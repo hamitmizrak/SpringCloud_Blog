c5da11fd5746df01cb5e363c399268441cfdc745
hamitmizrak <hamitmizrak@gmail.com>
2022-12-07 20:37:23 +0300

    Initial commit

---
+++ src/test/java/com/hamitmizrak/springcloud_blog/SpringCloudBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak.springcloud_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
